d1c8bdef2e708102dbe2eafb3ff6ef66571e7a24
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-05-06 16:57:37 +0200

    chore: Add CheckStyle

--- src/main/java/com/seek_with_sight/SeekWithSightApplication.java
+++ src/main/java/com/seek_with_sight/SeekWithSightApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SeekWithSightApplication {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(SeekWithSightApplication.class, args);
 	}
 }
