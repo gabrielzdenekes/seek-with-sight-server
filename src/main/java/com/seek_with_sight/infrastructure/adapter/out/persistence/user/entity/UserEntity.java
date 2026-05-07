a0e12c5a6b68177adb1890d00ceaba9318f9f284
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-05-07 19:58:25 +0200

    fix: Fix lint bad import *

--- src/main/java/com/seek_with_sight/infrastructure/adapter/out/persistence/user/entity/UserEntity.java
+++ src/main/java/com/seek_with_sight/infrastructure/adapter/out/persistence/user/entity/UserEntity.java
@@ -1,6 +1,10 @@
 package com.seek_with_sight.infrastructure.adapter.out.persistence.user.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
