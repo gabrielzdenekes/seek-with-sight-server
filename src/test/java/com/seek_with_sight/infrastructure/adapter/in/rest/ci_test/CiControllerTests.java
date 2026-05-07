00303b9e1df7497f9ec161d73735b7d1cee7f32a
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-05-07 18:46:41 +0200

    fix: Remove Container load from slice test

--- src/test/java/com/seek_with_sight/infrastructure/adapter/in/rest/ci_test/CiControllerTests.java
+++ src/test/java/com/seek_with_sight/infrastructure/adapter/in/rest/ci_test/CiControllerTests.java
@@ -1,18 +1,15 @@
 package com.seek_with_sight.infrastructure.adapter.in.rest.ci_test;
 
-import com.seek_with_sight.TestcontainersConfiguration;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Import(TestcontainersConfiguration.class)
 @AutoConfigureMockMvc(addFilters = false)
 @WebMvcTest(CiController.class)
 @Tag("slice-tests")
