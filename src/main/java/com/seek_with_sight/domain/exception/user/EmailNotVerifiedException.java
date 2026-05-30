6e05a44d83c60ced8175c000a5a3179e739d9675
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-05-30 14:25:42 +0200

    feature: Login prohibited if the user hasn't verified the email

--- src/main/java/com/seek_with_sight/domain/exception/user/EmailNotVerifiedException.java
+++ src/main/java/com/seek_with_sight/domain/exception/user/EmailNotVerifiedException.java
@@ -1,10 +1,10 @@
-package com.seek_with_sight.domain.exception.email;
+package com.seek_with_sight.domain.exception.user;
 
 import com.seek_with_sight.domain.exception.BusinessException;
 import com.seek_with_sight.domain.exception.ErrorType;
 
 public class EmailNotVerifiedException extends BusinessException {
     public EmailNotVerifiedException(Object... args) {
-        super("EMAIL_TOKEN_ALREADY_USED", "email.error.token-already-used", ErrorType.BUSINESS_RULE, args);
+        super("EMAIL_NOT_VERIFIED", "user.email.not-verified", ErrorType.FORBIDDEN, args);
     }
 }
