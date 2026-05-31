b337a36ee2ee206733b44a053fe3c6a1c7e1b59c
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-05-31 15:48:09 +0200

    feature: Create profile controllers

---
+++ src/main/java/com/seek_with_sight/infrastructure/adapter/in/rest/profile/CustomerProfileController.java
@@ -0,0 +1,11 @@
+package com.seek_with_sight.infrastructure.adapter.in.rest.profile;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/customer-profile")
+public class CustomerProfileController {
+}
