841f4852b00705ffa47a4697ebaba0cb48832b37
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-06-02 05:56:25 +0200

    feature: Implement controller

--- src/main/java/com/seek_with_sight/infrastructure/adapter/in/rest/profile/SellerProfileController.java
+++ src/main/java/com/seek_with_sight/infrastructure/adapter/in/rest/profile/SellerProfileController.java
@@ -1,6 +1,7 @@
 package com.seek_with_sight.infrastructure.adapter.in.rest.profile;
 
 import com.seek_with_sight.application.port.in.profile.CreateSellerProfileUseCase;
+import com.seek_with_sight.domain.model.user.User;
 import com.seek_with_sight.infrastructure.adapter.in.rest.profile.dto.CreateSellerRequest;
 import com.seek_with_sight.infrastructure.adapter.in.rest.profile.mapper.SellerProfileRestMapper;
 import jakarta.validation.Valid;
@@ -18,7 +19,8 @@ public class SellerProfileController {
     private final CreateSellerProfileUseCase createSellerProfileUseCase;
 
     @PostMapping
-    public void createSellerProfile(@Valid @RequestBody CreateSellerRequest createSellerRequest) {
-
+    public User createSellerProfile(@Valid @RequestBody CreateSellerRequest createSellerRequest) {
+        var createSellerCommand = mapper.toCreateSellerProfileCommand(createSellerRequest);
+        return createSellerProfileUseCase.createSellerProfile(createSellerCommand);
     }
 }
