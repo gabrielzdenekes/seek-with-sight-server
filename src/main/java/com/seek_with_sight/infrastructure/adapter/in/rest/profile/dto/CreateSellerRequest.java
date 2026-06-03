c7fa6146a651ae6ca4363534a7d218510ab4a1e8
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-06-03 06:14:21 +0200

    feature: Add validation messages for businessName property

--- src/main/java/com/seek_with_sight/infrastructure/adapter/in/rest/profile/dto/CreateSellerRequest.java
+++ src/main/java/com/seek_with_sight/infrastructure/adapter/in/rest/profile/dto/CreateSellerRequest.java
@@ -1,13 +1,18 @@
 package com.seek_with_sight.infrastructure.adapter.in.rest.profile.dto;
 
 import com.seek_with_sight.infrastructure.adapter.in.rest.user.dto.CreateUserRequest;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CreateSellerRequest extends CreateUserRequest {
+    @NotBlank(message = "{seller.business-name.required}")
+    @Size(max = 200, message = "{seller.business-name.max-length}")
     private String businessName;
+
     private String businessAddress;
     private String taxId;
 
