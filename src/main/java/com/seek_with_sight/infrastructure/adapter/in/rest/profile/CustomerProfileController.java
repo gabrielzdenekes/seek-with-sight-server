33f6f39ba361e8defe34341d4d913c8a9a3945e9
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-06-04 05:54:41 +0200

    feature: Create empty controller endpoints

--- src/main/java/com/seek_with_sight/infrastructure/adapter/in/rest/profile/CustomerProfileController.java
+++ src/main/java/com/seek_with_sight/infrastructure/adapter/in/rest/profile/CustomerProfileController.java
@@ -1,15 +1,20 @@
 package com.seek_with_sight.infrastructure.adapter.in.rest.profile;
 
 import com.seek_with_sight.application.port.in.profile.CreateCustomerProfileUseCase;
+import com.seek_with_sight.domain.model.profile.CustomerProfile;
 import com.seek_with_sight.domain.model.user.User;
 import com.seek_with_sight.infrastructure.adapter.in.rest.profile.dto.CreateCustomerRequest;
 import com.seek_with_sight.infrastructure.adapter.in.rest.profile.mapper.CustomerProfileRestMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,5 +27,10 @@ public class CustomerProfileController {
     public User createCustomerProfile(@Valid @RequestBody CreateCustomerRequest createCustomerRequest) {
         var command = mapper.toCreateCustomerProfileCommand(createCustomerRequest);
         return createCustomerProfileUseCase.createCustomerProfile(command);
+    }
+
+    @GetMapping
+    public CustomerProfile getById(@PathVariable UUID userId) {
+
     }
 }
--- src/main/java/com/seek_with_sight/infrastructure/adapter/in/rest/profile/SellerProfileController.java
+++ src/main/java/com/seek_with_sight/infrastructure/adapter/in/rest/profile/SellerProfileController.java
@@ -1,15 +1,20 @@
 package com.seek_with_sight.infrastructure.adapter.in.rest.profile;
 
 import com.seek_with_sight.application.port.in.profile.CreateSellerProfileUseCase;
+import com.seek_with_sight.domain.model.profile.SellerProfile;
 import com.seek_with_sight.domain.model.user.User;
 import com.seek_with_sight.infrastructure.adapter.in.rest.profile.dto.CreateSellerRequest;
 import com.seek_with_sight.infrastructure.adapter.in.rest.profile.mapper.SellerProfileRestMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,5 +27,10 @@ public class SellerProfileController {
     public User createSellerProfile(@Valid @RequestBody CreateSellerRequest createSellerRequest) {
         var createSellerCommand = mapper.toCreateSellerProfileCommand(createSellerRequest);
         return createSellerProfileUseCase.createSellerProfile(createSellerCommand);
+    }
+
+    @GetMapping
+    public SellerProfile getById(@PathVariable UUID userId) {
+
     }
 }
