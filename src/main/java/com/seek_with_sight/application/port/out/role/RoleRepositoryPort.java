ba4787be79ff402c0eb6586483e33c2ad34da800
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-06-06 15:17:49 +0200

    refactor: Create BasePersistenceAdapter and refactor customers, roles and sellers

--- src/main/java/com/seek_with_sight/application/port/out/role/RoleRepositoryPort.java
+++ src/main/java/com/seek_with_sight/application/port/out/role/RoleRepositoryPort.java
@@ -4,10 +4,7 @@ import com.seek_with_sight.domain.model.role.Role;
 import com.seek_with_sight.domain.model.role.RoleName;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface RoleRepositoryPort {
-    Optional<Role> findByName(RoleName name);
-
     List<Role> findByNameIn(List<RoleName> roleNames);
 }
--- src/main/java/com/seek_with_sight/infrastructure/adapter/out/persistence/role/RolePersistenceAdapter.java
+++ src/main/java/com/seek_with_sight/infrastructure/adapter/out/persistence/role/RolePersistenceAdapter.java
@@ -3,29 +3,27 @@ package com.seek_with_sight.infrastructure.adapter.out.persistence.role;
 import com.seek_with_sight.domain.model.role.Role;
 import com.seek_with_sight.domain.model.role.RoleName;
 import com.seek_with_sight.application.port.out.role.RoleRepositoryPort;
-import com.seek_with_sight.infrastructure.adapter.out.persistence.role.mapper.RolePersistenceMapper;
+import com.seek_with_sight.infrastructure.adapter.out.persistence.role.entity.RoleEntity;
 import com.seek_with_sight.infrastructure.adapter.out.persistence.role.repository.RoleJpaRepository;
-import lombok.RequiredArgsConstructor;
+import com.seek_with_sight.infrastructure.adapter.out.persistence.shared.BasePersistenceAdapter;
+import com.seek_with_sight.infrastructure.adapter.out.persistence.shared.PersistenceMapper;
 
 import java.util.List;
-import java.util.Optional;
 
-@RequiredArgsConstructor
-public class RolePersistenceAdapter implements RoleRepositoryPort {
-    private final RoleJpaRepository roleRepository;
-    private final RolePersistenceMapper roleMapper;
-
-    @Override
-    public Optional<Role> findByName(RoleName name) {
-        return roleRepository.findByName(name)
-                .map(roleMapper::toDomain);
+public class RolePersistenceAdapter
+        extends BasePersistenceAdapter<Role, RoleEntity, RoleJpaRepository>
+        implements RoleRepositoryPort {
+    public RolePersistenceAdapter(
+            RoleJpaRepository repository,
+            PersistenceMapper<Role, RoleEntity> mapper) {
+        super(repository, mapper, RoleEntity::new);
     }
 
     @Override
     public List<Role> findByNameIn(List<RoleName> roleNames) {
-        return roleRepository
+        return repository
                 .findByNameIn(roleNames).stream()
-                .map(roleMapper::toDomain)
+                .map(mapper::toDomain)
                 .toList();
     }
 }
