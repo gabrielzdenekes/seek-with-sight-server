fd1c594c8a95e61fbf664bcf0af493554e8cf681
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-06-10 05:56:57 +0200

    feature: Add ProductSEO domain model

--- src/main/java/com/seek_with_sight/domain/model/product/ProductSEO.java
+++ src/main/java/com/seek_with_sight/domain/model/product/ProductSEO.java
@@ -1,4 +1,85 @@
 package com.seek_with_sight.domain.model.product;
 
-public class ProductSEO {
+import com.seek_with_sight.domain.model.BaseDomainModel;
+
+public class ProductSEO extends BaseDomainModel {
+    private Product product;
+
+    private String metaTitle;
+
+    private String metaDescription;
+
+    private String metaKeywords;
+
+    private String canonicalUrl;
+
+    private String ogTitle;
+
+    private String ogDescription;
+
+    private String ogImageUrl;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String getMetaTitle() {
+        return metaTitle;
+    }
+
+    public void setMetaTitle(String metaTitle) {
+        this.metaTitle = metaTitle;
+    }
+
+    public String getMetaDescription() {
+        return metaDescription;
+    }
+
+    public void setMetaDescription(String metaDescription) {
+        this.metaDescription = metaDescription;
+    }
+
+    public String getMetaKeywords() {
+        return metaKeywords;
+    }
+
+    public void setMetaKeywords(String metaKeywords) {
+        this.metaKeywords = metaKeywords;
+    }
+
+    public String getCanonicalUrl() {
+        return canonicalUrl;
+    }
+
+    public void setCanonicalUrl(String canonicalUrl) {
+        this.canonicalUrl = canonicalUrl;
+    }
+
+    public String getOgTitle() {
+        return ogTitle;
+    }
+
+    public void setOgTitle(String ogTitle) {
+        this.ogTitle = ogTitle;
+    }
+
+    public String getOgDescription() {
+        return ogDescription;
+    }
+
+    public void setOgDescription(String ogDescription) {
+        this.ogDescription = ogDescription;
+    }
+
+    public String getOgImageUrl() {
+        return ogImageUrl;
+    }
+
+    public void setOgImageUrl(String ogImageUrl) {
+        this.ogImageUrl = ogImageUrl;
+    }
 }
