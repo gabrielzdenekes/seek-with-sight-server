b4fe9763ab1128d3a6912492485c8efa7fd9ad32
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-06-10 05:51:46 +0200

    feature: Add ProductTag getters and setters

--- src/main/java/com/seek_with_sight/domain/model/product/ProductTag.java
+++ src/main/java/com/seek_with_sight/domain/model/product/ProductTag.java
@@ -10,4 +10,28 @@ public class ProductTag extends BaseDomainModel {
     private String slug;
 
     private List<Product> products;
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
