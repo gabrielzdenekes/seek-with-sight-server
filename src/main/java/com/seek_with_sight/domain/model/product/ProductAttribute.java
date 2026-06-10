b7520a059c81dc92395937f5434da1f55e70799f
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-06-10 05:54:09 +0200

    feature: Add ProductAttribute domain model

--- src/main/java/com/seek_with_sight/domain/model/product/ProductAttribute.java
+++ src/main/java/com/seek_with_sight/domain/model/product/ProductAttribute.java
@@ -1,4 +1,65 @@
 package com.seek_with_sight.domain.model.product;
 
-public class ProductAttribute {
+import com.seek_with_sight.domain.model.BaseDomainModel;
+
+public class ProductAttribute extends BaseDomainModel {
+    private Product product;
+
+    private String name;
+
+    private String value;
+
+    private String unit;
+
+    private Boolean isFilterable;
+
+    private Integer sortOrder;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public Boolean getFilterable() {
+        return isFilterable;
+    }
+
+    public void setFilterable(Boolean filterable) {
+        isFilterable = filterable;
+    }
+
+    public Integer getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(Integer sortOrder) {
+        this.sortOrder = sortOrder;
+    }
 }
