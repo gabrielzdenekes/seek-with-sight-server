3a2a14b73d354f3b63e62b1b3f96a6614c9c1ebb
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-06-14 06:24:24 +0200

    feature: add ProductSeoDto validation messages

--- src/main/java/com/seek_with_sight/product/infrastructure/adapter/in/rest/dto/ProductSeoDto.java
+++ src/main/java/com/seek_with_sight/product/infrastructure/adapter/in/rest/dto/ProductSeoDto.java
@@ -1,9 +1,6 @@
 package com.seek_with_sight.product.infrastructure.adapter.in.rest.dto;
 
-import com.seek_with_sight.product.domain.model.Product;
-
 public record ProductSeoDto(
-        Product product,
         String metaTitle,
         String metaDescription,
         String canonicalUrl,
