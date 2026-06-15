e01a2cc9ad601be5b308f3671c87cbeefb42a308
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-06-15 05:43:18 +0200

    Add create() method of CreateProductUseCase

--- src/main/java/com/seek_with_sight/product/application/port/in/create/CreateProductUseCase.java
+++ src/main/java/com/seek_with_sight/product/application/port/in/create/CreateProductUseCase.java
@@ -1,4 +1,8 @@
 package com.seek_with_sight.product.application.port.in.create;
 
+import com.seek_with_sight.product.application.port.in.create.command.CreateProductCommand;
+import com.seek_with_sight.product.domain.model.Product;
+
 public interface CreateProductUseCase {
+    Product create(CreateProductCommand command);
 }
