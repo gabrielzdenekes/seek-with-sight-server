f8f476c25fab03c2e22ba0d0cbe678a1a8506df0
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-06-16 05:26:11 +0200

    Create categories.json for initial seeding

---
+++ src/main/java/com/seek_with_sight/product/infrastructure/adapter/out/persistence/initializer/CategoriesInitializer.java
@@ -0,0 +1,9 @@
+package com.seek_with_sight.product.infrastructure.adapter.out.persistence.initializer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CategoriesInitializer {
+}
