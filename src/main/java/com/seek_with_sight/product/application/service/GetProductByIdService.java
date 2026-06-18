0400133fd98257ed9144ce30245544cab28c9a0c
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-06-18 17:54:38 +0200

    Add getById method to repository

--- src/main/java/com/seek_with_sight/product/application/service/GetProductByIdService.java
+++ src/main/java/com/seek_with_sight/product/application/service/GetProductByIdService.java
@@ -1,13 +1,18 @@
 package com.seek_with_sight.product.application.service;
 
 import com.seek_with_sight.product.application.port.in.GetProductByIdUseCase;
+import com.seek_with_sight.product.application.port.out.ProductRepositoryPort;
 import com.seek_with_sight.product.domain.model.Product;
 
 import java.util.UUID;
 
 public class GetProductByIdService implements GetProductByIdUseCase {
+    private ProductRepositoryPort productRepo;
+
     @Override
     public Product getById(UUID id) {
-        return null;
+        return productRepo
+                .getById(id)
+                .orElseThrow();
     }
 }
--- src/main/java/com/seek_with_sight/product/infrastructure/adapter/out/persistence/ProductPersistenceAdapter.java
+++ src/main/java/com/seek_with_sight/product/infrastructure/adapter/out/persistence/ProductPersistenceAdapter.java
@@ -7,11 +7,19 @@ import com.seek_with_sight.product.infrastructure.adapter.out.persistence.mapper
 import com.seek_with_sight.product.infrastructure.adapter.out.persistence.repository.ProductJpaRepository;
 import com.seek_with_sight.shared.infrastructure.adapter.out.persistence.BasePersistenceAdapter;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public class ProductPersistenceAdapter
         extends BasePersistenceAdapter<Product, ProductEntity, ProductJpaRepository>
         implements ProductRepositoryPort {
 
     public ProductPersistenceAdapter(ProductJpaRepository repository, ProductPersistenceMapper mapper) {
         super(repository, mapper, ProductEntity::new);
+    }
+
+    @Override
+    public Optional<Product> getById(UUID id) {
+        return Optional.empty();
     }
 }
