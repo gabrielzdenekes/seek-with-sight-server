57c61587481828585c7fd0b579e408b309b6a823
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-06-21 06:39:35 +0200

    Reuse test containers

--- src/test/java/com/seek_with_sight/utils/IntegrationTestsBase.java
+++ src/test/java/com/seek_with_sight/utils/IntegrationTestsBase.java
@@ -1,11 +1,11 @@
 package com.seek_with_sight.utils;
 
-import com.seek_with_sight.TestcontainersConfiguration;
+import com.seek_with_sight.TestsConfiguration;
 import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
+@Import(TestsConfiguration.class)
 @SpringBootTest
 @Tag("integration-tests")
 public abstract class IntegrationTestsBase extends TestsBase {
