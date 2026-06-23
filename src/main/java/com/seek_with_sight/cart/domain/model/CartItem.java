df2dcaf14b430c1001bd5ec88b28ee2dc0d22b12
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-06-23 05:47:10 +0200

    Change domain models

--- src/main/java/com/seek_with_sight/cart/domain/model/CartItem.java
+++ src/main/java/com/seek_with_sight/cart/domain/model/CartItem.java
@@ -6,5 +6,37 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 public class CartItem extends BaseDomainModel {
+    private UUID productId;
 
+    private Integer quantity;
+
+    private BigDecimal price;
+
+    public BigDecimal getSubTotal() {
+        return price.multiply(BigDecimal.valueOf(quantity));
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public void setProductId(UUID productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
