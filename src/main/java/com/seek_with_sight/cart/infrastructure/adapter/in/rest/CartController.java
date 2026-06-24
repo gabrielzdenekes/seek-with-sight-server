a81a3795c7740bc7dcb20b591a9d9e072231d244
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-06-24 15:02:21 +0200

    Create controller method and request dto

--- src/main/java/com/seek_with_sight/cart/infrastructure/adapter/in/rest/CartController.java
+++ src/main/java/com/seek_with_sight/cart/infrastructure/adapter/in/rest/CartController.java
@@ -1,12 +1,18 @@
 package com.seek_with_sight.cart.infrastructure.adapter.in.rest;
 
 import com.seek_with_sight.cart.application.port.in.FindCartForCurrentUser;
+import com.seek_with_sight.cart.infrastructure.adapter.in.rest.dto.AddCartItemRequest;
 import com.seek_with_sight.cart.infrastructure.adapter.in.rest.dto.CartResponse;
 import com.seek_with_sight.cart.infrastructure.adapter.in.rest.mapper.CartRestMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +26,11 @@ public class CartController {
     public CartResponse get(Authentication authentication) {
         var cart = findCartForCurrentUser.find();
         return mapper.toCartResponse(cart);
+    }
+
+    @PostMapping("/items")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void addItemToCart(@Valid @RequestBody AddCartItemRequest request) {
+
     }
 }
