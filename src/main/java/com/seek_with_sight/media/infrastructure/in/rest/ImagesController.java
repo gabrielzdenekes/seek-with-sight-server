259a1500313236186960d77e88f391a52b49fdb3
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-06-30 19:58:47 +0200

    Create empty ImagesController

---
+++ src/main/java/com/seek_with_sight/media/infrastructure/in/rest/ImagesController.java
@@ -0,0 +1,11 @@
+package com.seek_with_sight.media.infrastructure.in.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/images")
+@RequiredArgsConstructor
+public class ImagesController {
+}
