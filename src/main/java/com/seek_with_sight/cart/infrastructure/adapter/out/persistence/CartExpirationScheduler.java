1a1fc2110785a5eb455ff7c70ec61baed1b8fc22
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-07-10 06:42:05 +0200

    Create cart cleanup scheduler

---
+++ src/main/java/com/seek_with_sight/cart/infrastructure/adapter/out/persistence/CartExpirationScheduler.java
@@ -0,0 +1,36 @@
+package com.seek_with_sight.cart.infrastructure.adapter.out.persistence;
+
+import com.seek_with_sight.cart.infrastructure.adapter.out.persistence.repository.CartJpaRepository;
+import com.seek_with_sight.product.application.port.in.product.ReleaseStockUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@Component
+@RequiredArgsConstructor
+public class CartExpirationScheduler {
+    private final CartJpaRepository cartRepository;
+    private final ReleaseStockUseCase releaseStockUseCase;
+
+    // Runed every 5 min
+    @Scheduled(fixedRate = 300000)
+    public void cleanupAbandonedCarts() {
+        var threshold = LocalDateTime.now()
+                .minusMinutes(15)
+                .atZone(ZoneId.systemDefault())
+                .toInstant();
+
+        var abandonedCarts = cartRepository.findALlByUpdatedAtBefore(threshold);
+
+        for (var cart : abandonedCarts) {
+            for (var item : cart.getItems()) {
+                releaseStockUseCase.release(item.getVariant().getId(), item.getQuantity());
+            }
+
+            cartRepository.delete(cart);
+        }
+    }
+}
--- src/main/java/com/seek_with_sight/cart/infrastructure/adapter/out/persistence/repository/CartJpaRepository.java
+++ src/main/java/com/seek_with_sight/cart/infrastructure/adapter/out/persistence/repository/CartJpaRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,4 +21,9 @@ public interface CartJpaRepository extends JpaRepository<CartEntity, UUID> {
     Optional<CartEntity> findWithItemsByUserId(UUID userId);
 
     boolean existsByUserId(Long userId);
+
+    @EntityGraph(attributePaths = {
+            "items.variant"
+    })
+    List<CartEntity> findALlByUpdatedAtBefore(Instant updatedBefore);
 }
