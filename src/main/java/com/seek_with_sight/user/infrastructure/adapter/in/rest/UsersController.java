30623d97a551ab0cf8f14c62388a8ea2414d3e34
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-07-12 16:19:31 +0200

    Remove ApiResponse details

--- src/main/java/com/seek_with_sight/user/infrastructure/adapter/in/rest/UsersController.java
+++ src/main/java/com/seek_with_sight/user/infrastructure/adapter/in/rest/UsersController.java
@@ -2,13 +2,11 @@ package com.seek_with_sight.user.infrastructure.adapter.in.rest;
 
 import com.seek_with_sight.user.application.port.in.CreateUserUseCase;
 import com.seek_with_sight.authorization.domain.model.role.RoleName;
-import com.seek_with_sight.shared.infrastructure.adapter.in.rest.annotation.ApiResponseDetails;
 import com.seek_with_sight.user.infrastructure.adapter.in.rest.dto.CreateUserRequest;
 import com.seek_with_sight.user.infrastructure.adapter.in.rest.dto.UserResponse;
 import com.seek_with_sight.user.infrastructure.adapter.in.rest.mapper.UserRestMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +22,6 @@ public class UsersController {
     private final UserRestMapper mapper;
 
     @PostMapping
-    @ApiResponseDetails(messageCode = "user.created", status =  HttpStatus.CREATED)
     public UserResponse createUser(@Valid @RequestBody CreateUserRequest userRequest) {
         var createUserCommand = mapper.fromRequestToCreateCommand(userRequest);
         var createdUser = createUserUseCase.createUser(createUserCommand, List.of(RoleName.ROLE_CUSTOMER));
