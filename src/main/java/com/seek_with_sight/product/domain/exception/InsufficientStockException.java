8741a50899a121133626249e9e1fdec29adbc95c
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-07-12 15:14:23 +0200

    Refactor InsufficientStockException

--- src/main/java/com/seek_with_sight/product/domain/exception/InsufficientStockException.java
+++ src/main/java/com/seek_with_sight/product/domain/exception/InsufficientStockException.java
@@ -4,7 +4,12 @@ import com.seek_with_sight.shared.domain.exception.BusinessException;
 import com.seek_with_sight.shared.domain.exception.ErrorType;
 
 public class InsufficientStockException extends BusinessException {
-    public InsufficientStockException(Object[] args) {
-        super("INSUFFICIENT_STOCK", "inventory.error.insufficient-stock", ErrorType.BUSINESS_RULE, args);
+    public InsufficientStockException(Object... args) {
+        super(
+                "INSUFFICIENT_STOCK",
+                ErrorType.BUSINESS,
+                "Insufficient stock for variant with ID %s. Actual stock: %s. Required stock for reserve: %s",
+                args
+        );
     }
 }
