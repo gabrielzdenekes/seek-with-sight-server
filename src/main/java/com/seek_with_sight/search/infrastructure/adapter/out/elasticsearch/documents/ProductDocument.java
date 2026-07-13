8cdc65032a8b4ca0ae87a05a5b4d8b9db9ffd627
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-07-13 04:54:50 +0200

    Add ProductDocument with basic search properties

---
+++ src/main/java/com/seek_with_sight/search/infrastructure/adapter/out/elasticsearch/documents/ProductDocument.java
@@ -0,0 +1,33 @@
+package com.seek_with_sight.search.infrastructure.adapter.out.elasticsearch.documents;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.UUID;
+
+@Document(indexName = "products")
+@Getter
+@Setter
+public class ProductDocument {
+    @Id
+    private UUID id;
+
+    @Field(type = FieldType.Text)
+    private String name;
+
+    @Field(type = FieldType.Keyword)
+    private String slug;
+
+    @Field(type = FieldType.Text)
+    private String description;
+
+    @Field(type = FieldType.Keyword)
+    private String categoryName;
+
+    @Field(type = FieldType.Keyword)
+    private String brandName;
+}
