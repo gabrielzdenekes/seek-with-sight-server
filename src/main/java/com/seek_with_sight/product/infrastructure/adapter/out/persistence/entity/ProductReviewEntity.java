c790f1752f2da46ed8ee4e109b7a1e3d0047a151
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-07-14 18:53:52 +0200

    Create ProductReviewEntity and add review properties to ProductEntity

---
+++ src/main/java/com/seek_with_sight/product/infrastructure/adapter/out/persistence/entity/ProductReviewEntity.java
@@ -0,0 +1,40 @@
+package com.seek_with_sight.product.infrastructure.adapter.out.persistence.entity;
+
+import com.seek_with_sight.shared.infrastructure.adapter.out.persistence.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "product_reviews", indexes = {
+        @Index(name = "idx_review_product", columnList = "product_id"),
+        @Index(name = "idx_review_user", columnList = "user_id"),
+})
+public class ProductReviewEntity extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private ProductEntity product;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false)
+    private Integer count;
+
+    @Column(length = 150)
+    private String title;
+
+    @Column(length = 1000)
+    private String comment;
+}
