2bc2b93ee9348cb8406f5ac6cd15a27c208b23a6
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-07-17 05:19:26 +0200

    Create jpa repository

---
+++ src/main/java/com/seek_with_sight/order/infrastructure/out/persistence/repository/OrderJpaRepository.java
@@ -0,0 +1,11 @@
+package com.seek_with_sight.order.infrastructure.out.persistence.repository;
+
+import com.seek_with_sight.order.infrastructure.out.persistence.entity.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderJpaRepository extends JpaRepository<OrderEntity, UUID> {
+}
