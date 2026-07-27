29f7951941e344035179c6be7f61e71d577eaed6
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-07-27 05:03:17 +0200

    Fix ItemAlreadyAddedToCartException

--- src/main/java/com/seek_with_sight/cart/domain/exception/ItemAlreadyAddedToCartException.java
+++ src/main/java/com/seek_with_sight/cart/domain/exception/ItemAlreadyAddedToCartException.java
@@ -1,12 +1,13 @@
 package com.seek_with_sight.cart.domain.exception;
 
 import com.seek_with_sight.shared.domain.exception.BusinessException;
+import com.seek_with_sight.shared.domain.exception.ErrorCode;
 import com.seek_with_sight.shared.domain.exception.ErrorType;
 
 public class ItemAlreadyAddedToCartException extends BusinessException {
     public ItemAlreadyAddedToCartException(Object... args) {
         super(
-                "ITEM_ALREADY_ADDED_TO_CART",
+                ErrorCode.ITEM_ALREADY_ADDED_TO_CART,
                 ErrorType.BUSINESS,
                 "Variant with ID %s already added to the cart",
                 args
