2eca7c9029193da1c6cd32dc23db3b9c5c9d2413
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-07-27 05:07:35 +0200

    Fix EmailNotVerifiedException

--- src/main/java/com/seek_with_sight/user/domain/exception/EmailNotVerifiedException.java
+++ src/main/java/com/seek_with_sight/user/domain/exception/EmailNotVerifiedException.java
@@ -1,10 +1,11 @@
 package com.seek_with_sight.user.domain.exception;
 
 import com.seek_with_sight.shared.domain.exception.BusinessException;
+import com.seek_with_sight.shared.domain.exception.ErrorCode;
 import com.seek_with_sight.shared.domain.exception.ErrorType;
 
 public class EmailNotVerifiedException extends BusinessException {
     public EmailNotVerifiedException(Object... args) {
-        super("EMAIL_NOT_VERIFIED", ErrorType.FORBIDDEN, "User with email %s is not verified", args);
+        super(ErrorCode.EMAIL_NOT_VERIFIED, ErrorType.FORBIDDEN, "User with email %s is not verified", args);
     }
 }
