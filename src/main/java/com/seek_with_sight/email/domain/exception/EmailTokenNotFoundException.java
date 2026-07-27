e536e41353e358b373de35b7ecc984caa946390a
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-07-27 05:04:42 +0200

    Fix EmailTokenNotFoundException

--- src/main/java/com/seek_with_sight/email/domain/exception/EmailTokenNotFoundException.java
+++ src/main/java/com/seek_with_sight/email/domain/exception/EmailTokenNotFoundException.java
@@ -1,10 +1,11 @@
 package com.seek_with_sight.email.domain.exception;
 
 import com.seek_with_sight.shared.domain.exception.BusinessException;
+import com.seek_with_sight.shared.domain.exception.ErrorCode;
 import com.seek_with_sight.shared.domain.exception.ErrorType;
 
 public class EmailTokenNotFoundException extends BusinessException {
     public EmailTokenNotFoundException(Object... args) {
-        super("EMAIL_TOKEN_NOT_FOUND", ErrorType.NOT_FOUND, "Email token not found", args);
+        super(ErrorCode.EMAIL_TOKEN_NOT_FOUND, ErrorType.NOT_FOUND, "Email token not found", args);
     }
 }
