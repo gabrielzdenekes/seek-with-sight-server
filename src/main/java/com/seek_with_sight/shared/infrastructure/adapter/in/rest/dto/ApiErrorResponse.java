b8700e74b5f8c9392c9943a4fff9be78be441de3
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-07-27 05:08:56 +0200

    Change error code property of ApiErrorResponse

--- src/main/java/com/seek_with_sight/shared/infrastructure/adapter/in/rest/dto/ApiErrorResponse.java
+++ src/main/java/com/seek_with_sight/shared/infrastructure/adapter/in/rest/dto/ApiErrorResponse.java
@@ -1,12 +1,13 @@
 package com.seek_with_sight.shared.infrastructure.adapter.in.rest.dto;
 
+import com.seek_with_sight.shared.domain.exception.ErrorCode;
 import lombok.Getter;
 
 @Getter
 public class ApiErrorResponse<T> extends ApiResponse<T> {
-    private final String errorCode;
+    private final ErrorCode errorCode;
 
-    public ApiErrorResponse(String message, T data, String errorCode, int status) {
+    public ApiErrorResponse(String message, T data, ErrorCode errorCode, int status) {
         super(message, data, false, status);
         this.errorCode = errorCode;
     }
@@ -14,7 +15,7 @@ public class ApiErrorResponse<T> extends ApiResponse<T> {
     public static <T> ApiErrorResponse<T> create(
             String message,
             T data,
-            String errorCode,
+            ErrorCode errorCode,
             int status
     ) {
         return new ApiErrorResponse<>(message, data, errorCode, status);
