2855eb8da941ac92358ca28bf406c9580f20304d
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-07-27 05:02:44 +0200

    Fix CartItemNotFoundException

--- src/main/java/com/seek_with_sight/cart/domain/exception/CartItemNotFoundException.java
+++ src/main/java/com/seek_with_sight/cart/domain/exception/CartItemNotFoundException.java
@@ -1,10 +1,11 @@
 package com.seek_with_sight.cart.domain.exception;
 
 import com.seek_with_sight.shared.domain.exception.BusinessException;
+import com.seek_with_sight.shared.domain.exception.ErrorCode;
 import com.seek_with_sight.shared.domain.exception.ErrorType;
 
 public class CartItemNotFoundException extends BusinessException {
     public CartItemNotFoundException(Object... args) {
-        super("CART_ITEM_NOT_FOUND", ErrorType.BUSINESS, "Cart item with %s not found", args);
+        super(ErrorCode.CART_ITEM_NOT_FOUND, ErrorType.BUSINESS, "Cart item with %s not found", args);
     }
 }
