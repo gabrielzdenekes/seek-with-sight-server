ccd371acd4beee0a7ddff76ae304708fb96c8765
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-07-30 06:16:39 +0200

    Add cache

--- src/main/java/com/seek_with_sight/product/application/service/category/GetCategoryTreeService.java
+++ src/main/java/com/seek_with_sight/product/application/service/category/GetCategoryTreeService.java
@@ -4,6 +4,7 @@ import com.seek_with_sight.product.application.port.in.category.CategoryListItem
 import com.seek_with_sight.product.application.port.in.category.GetCategoryTreeUseCase;
 import com.seek_with_sight.product.application.port.out.CategoryRepositoryPort;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ public class GetCategoryTreeService implements GetCategoryTreeUseCase {
     private final CategoryRepositoryPort repo;
 
     @Override
+    @Cacheable(value = "categoriesTree")
     public List<CategoryListItem> get() {
         return repo.getCategoryTree();
     }
