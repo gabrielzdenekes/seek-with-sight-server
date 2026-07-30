384ae805ecf4ecdb0b1e3136c45e5532e6e436d1
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-07-30 06:06:16 +0200

    Fetch all categories from the endpoint

--- src/main/java/com/seek_with_sight/product/infrastructure/adapter/in/rest/CategoriesController.java
+++ src/main/java/com/seek_with_sight/product/infrastructure/adapter/in/rest/CategoriesController.java
@@ -1,12 +1,23 @@
 package com.seek_with_sight.product.infrastructure.adapter.in.rest;
 
+import com.seek_with_sight.product.application.port.in.category.CategoryListItem;
+import com.seek_with_sight.product.application.port.in.category.GetCategoriesUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
 @RequiredArgsConstructor
 public class CategoriesController {
+    private final GetCategoriesUseCase getCategoriesUseCase;
 
+    @GetMapping
+    public List<CategoryListItem> getCategoryTree() {
+        var tree = getCategoriesUseCase.get();
+        return tree;
+    }
 }
