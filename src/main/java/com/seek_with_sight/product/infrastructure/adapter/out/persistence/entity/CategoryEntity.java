623afaaf79d07671f49e57130d6ec656ab65ee9d
Gabriel Belchev <gabriel.zdenek.es@gmail.com>
2026-08-02 14:31:07 +0200

    Add indexes to category

--- src/main/java/com/seek_with_sight/product/infrastructure/adapter/out/persistence/entity/CategoryEntity.java
+++ src/main/java/com/seek_with_sight/product/infrastructure/adapter/out/persistence/entity/CategoryEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
@@ -19,7 +20,13 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-@Table(name = "categories")
+@Table(
+        name = "categories",
+        indexes = {
+                @Index(name = "idx_categories_name", columnList = "name"),
+                @Index(name = "idx_categories_name_active", columnList = "name, is_active")
+        }
+)
 public class CategoryEntity extends BaseEntity {
     @Column(nullable = false, length = 150)
     private String name;
